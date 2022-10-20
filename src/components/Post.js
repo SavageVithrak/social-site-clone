275a934608c3ef417b19afb6e474572693260820
rossw01 <56947241+rossw01@users.noreply.github.com>
2022-10-20 11:44:23 +0100

    Added comment box #10

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import "./Post.css";
 
 function Post(props) {
+	const [comments, changesComments] = useState([
+		{ name: "Derek", comment: "Wow that is amazing" },
+		{ name: "Bill Gates", comment: "Testtttt" },
+	]);
+
+	const buildComments = () => {
+		let createdComments = comments.map((comment) => {
+			return (
+				<p>
+					{comment.name}: {comment.comment}
+				</p>
+			);
+		});
+		return createdComments;
+	};
+
 	return (
 		<div>
 			<Card className="card" border="primary">
@@ -12,6 +28,8 @@ function Post(props) {
 					<Card.Text>{props.body}</Card.Text>
 					<Card.Img className="img-max-size" src={props.image} />
 				</Card.Body>
+				<label>Comments:</label>
+				<div className="comment-box">{buildComments()}</div>
 				<div className="like-comment">
 					<Button variant="primary">Like</Button>
 					<input></input>
